fbe8f4b50c95f307fcd6c2186f867a65ea430dbf
unknown <lourdestuk@gmail.com>
2017-05-09 15:21:53 -0300

    aca tenes el slider papeeee'

--- script.js
+++ script.js
@@ -5,19 +5,36 @@ $(document).ready(function(){
 function initeSlider(){
 	var page = 0;
 	var totalPages = $(".sliderSheet").length;
+	console.log($(".next"));
+	$(".next").click(function(){
+		goTo(false);
+	});
+	$(".preview").click(function(){
+		goTo(true);
+	})
 	window.setInterval(function(){
 		var lastPage = page;
 		page = page >= 4 ? 0 : page + 1;
-		goTo(page, lastPage);
-	},2000)
+		goTo();
+	},6000)
 }
-function goTo(page){
-	$(".pagesContainer").animate({
-		left: "-100%"
-	},300, function(){
-		$(".pagesContainer").append($(".sliderSheet")[0]);
+function goTo(reverse){
+	if (reverse){
+		$(".pagesContainer").prepend($(".sliderSheet")[$(".sliderSheet").length-1]);
 		$(".pagesContainer").css({
+			left: "-100%"
+		})
+		$(".pagesContainer").animate({
 			left: 0
-		})		
-	})
+		},300)
+	} else {
+		$(".pagesContainer").animate({
+			left: "-100%"
+		},300, function(){
+			$(".pagesContainer").append($(".sliderSheet")[0]);
+			$(".pagesContainer").css({
+				left: 0
+			})		
+		})
+	}
 }
